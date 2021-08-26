4c0614407bf1dbfcded238167a4fc6bcb8d41a52
Jonathan Gordon <jgordon@jonathangordon.org>
2021-08-26 12:19:40 -0400

    Format Python

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import unittest
+
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('/autograder/source/instructor/tests')
-    with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(visibility='visible', stream=f).run(suite)
+if __name__ == "__main__":
+    suite = unittest.defaultTestLoader.discover(
+        "/autograder/source/instructor/tests"
+    )
+    with open("/autograder/results/results.json", "w") as f:
+        JSONTestRunner(visibility="visible", stream=f).run(suite)
